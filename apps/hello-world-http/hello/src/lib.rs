3208a6eb638f7ed101c71f8e879b9a9a716206f1
Enej Simrajh <enej.simrajh@gmail.com>
2023-09-14 01:11:46 +0200

    Http app changes

--- apps/hello-world-http/hello/src/lib.rs
+++ apps/hello-world-http/hello/src/lib.rs
@@ -11,5 +11,5 @@ fn handle_hello(req: Request) -> Result<Response> {
     Ok(http::Response::builder()
         .status(200)
         .header("foo", "bar")
-        .body(Some("Hello, Fermyon".into()))?)
+        .body(Some("Hello".into()))?)
 }
--- apps/hello-world-http/world/src/lib.rs
+++ apps/hello-world-http/world/src/lib.rs
@@ -11,5 +11,5 @@ fn handle_world(req: Request) -> Result<Response> {
     Ok(http::Response::builder()
         .status(200)
         .header("foo", "bar")
-        .body(Some("Hello, Fermyon".into()))?)
+        .body(Some("world".into()))?)
 }
