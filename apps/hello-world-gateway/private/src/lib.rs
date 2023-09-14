2eb73ffd491100edd2ee2e12697a972e22b92672
Enej Simrajh <enej.simrajh@gmail.com>
2023-09-15 00:47:59 +0200

    Added cors config

--- apps/hello-world-gateway/private/src/lib.rs
+++ apps/hello-world-gateway/private/src/lib.rs
@@ -1,15 +1,42 @@
 use anyhow::Result;
+use http::response::Builder;
 use spin_sdk::{
-    http::{Request, Response},
+    config,
+    http::{Params, Request, Response, Router},
     http_component,
 };
 
 /// A simple Spin HTTP component.
 #[http_component]
 fn handle_private(req: Request) -> Result<Response> {
-    println!("{:?}", req.headers());
-    Ok(http::Response::builder()
-        .status(200)
-        .header("foo", "bar")
-        .body(Some("Hello, Fermyon".into()))?)
+    let mut router = Router::default();
+    //router.get("/hello-world", search::search);
+    router.add("/...", http::Method::OPTIONS, process_preflight);
+    router.handle(req)
+}
+
+fn builder_with_cors(origin: String, methods: String, status: http::StatusCode) -> Builder {
+    http::Response::builder()
+        .status(status)
+        .header("Access-Control-Allow-Origin", origin)
+        .header("Access-Control-Allow-Methods", methods)
+        .header("Access-Control-Allow-Headers", "*")
+}
+
+pub(crate) fn process_preflight(_req: Request, _params: Params) -> Result<Response> {
+    let Ok(origin) = config::get("cors_origin") else {
+        println!("[ERROR]: Could not find CORS origin");
+        return Ok(http::Response::builder()
+            .status(http::StatusCode::INTERNAL_SERVER_ERROR)
+            .body(None)?);
+    };
+
+    let Ok(methods) = config::get("cors_methods") else {
+        println!("[ERROR]: Could not find CORS methods");
+        return Ok(http::Response::builder()
+            .status(http::StatusCode::INTERNAL_SERVER_ERROR)
+            .body(None)?);
+    };
+
+    Ok(builder_with_cors(origin, methods, http::StatusCode::OK).body(None)?)
 }
